ffe5f9ab9a499ec6c060d2a72b048cb0a604fb5b
qianyunyang <2997272453@qq.com>
2019-06-29 15:45:41 +0800

    qianyunyang01

---
+++ j1902_0629_usermanager/src/main/java/com/qf/j1902/pojo/User.java
@@ -0,0 +1,22 @@
+package com.qf.j1902.pojo;
+
+import lombok.Data;
+
+/**
+ * Created by Administrator on 2019/6/29.
+ */
+@Data
+public class User {
+    private int userid;
+    private String username;
+    private String upassword;
+    private String userimage;
+    private String realname;
+    private String gender;
+    private String telphone;
+    private String weichart;
+    private String tenqq;
+    private String useremail;
+    private String address;
+    private String userinfo;
+}
