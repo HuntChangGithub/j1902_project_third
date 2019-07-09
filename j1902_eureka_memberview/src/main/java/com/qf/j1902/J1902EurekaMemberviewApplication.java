171c75c530e819fe76b08f10089fb6dea3f7f728
zhaoguolingithub <ezhaoguolin@163.com>
2019-07-09 09:11:46 +0800

    zhaoguolin-0709

--- j1902_eureka_memberview/src/main/java/com/qf/j1902/J1902EurekaMemberviewApplication.java
+++ j1902_eureka_memberview/src/main/java/com/qf/j1902/J1902EurekaMemberviewApplication.java
@@ -2,7 +2,11 @@ package com.qf.j1902;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 public class J1902EurekaMemberviewApplication {
 
